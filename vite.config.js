44645736b27583f138f74653e65b4e29f7a83269
Uqyanzie Bintang <uqyanzie@gmail.com>
2023-08-14 03:22:52 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/agpot',
-  }
-
-  return config
-})
+export default defineConfig({
+  plugins: [react()],
+  base: "/agpot/"
+});
